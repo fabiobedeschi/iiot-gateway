ce41db17d1face09fe12215e62c5e92104333303
Paolo7297 <ca.paolo97@gmail.com>
2021-02-13 16:17:07 +0100

    First remote test

--- src/main.py
+++ src/main.py
@@ -3,6 +3,14 @@ from flask.helpers import send_from_directory
 
 app = Flask(__name__)
 
+@app.route('/users/<string:userid>', methods=['GET'])
+def get_companies(userid):
+    if userid == "FFFFFFFFFFFFFFFF0001020304050607": #TODO: Check if exists in DB
+        return userid, 200
+    else:
+        return userid, 403
+    return userid
+
 
 @app.route('/')
 def hello_world():
