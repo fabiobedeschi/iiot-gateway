ab079813079afd0602ff35fcda5085fc1a847442
Paolo7297 <ca.paolo97@gmail.com>
2021-02-15 21:13:15 +0100

    add sample api for patch requests

--- server/src/main.py
+++ server/src/main.py
@@ -1,15 +1,28 @@
 from flask import Flask
+from flask import request
 from flask.helpers import send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/users/<string:userid>', methods=['GET'])
-def get_companies(userid):
-    if userid == "FFFFFFFFFFFFFFFF0001020304050607": #TODO: Check if exists in DB
+def check_user(userid): #TODO: Check if exists in DB
+    if userid == "FFFFFFFFFFFFFFFF0001020304050607":
         return userid, 200
     else:
         return userid, 403
     return userid
+
+
+@app.route('/users/<string:userid>/update', methods=['PATCH'])
+def update_user(userid): #TODO: Update new value in DB to track 
+    data = request.json
+    s =""
+
+    for n in data :
+        a = data[n]
+        print(n,"è",a,sep=" ")
+        s = ( "%s<br>%s  è %s " ) % (s, n, a)
+    return s, 200
 
 
 @app.route('/')
