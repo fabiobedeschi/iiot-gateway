8601797d3d77d63c56322a2fa0c4f9dc0f160a69
Fabio Bedeschi <fabiobedeschi5@gmail.com>
2021-02-17 11:57:55 +0100

    update api endpoints

--- server/src/main.py
+++ server/src/main.py
@@ -4,28 +4,40 @@ from flask.helpers import send_from_directory
 
 app = Flask(__name__)
 
-@app.route('/users/<string:userid>', methods=['GET'])
-def check_user(userid): #TODO: Check if exists in DB
-    if userid == "FFFFFFFFFFFFFFFF0001020304050607":
-        return userid, 200
+
+@app.route('/users/<string:user_id>', methods=['GET'])
+def check_user(user_id):  # TODO: Check if exists in DB
+    if user_id == "FFFFFFFFFFFFFFFF0001020304050607":
+        return user_id, 200
     else:
-        return userid, 403
-    return userid
+        return user_id, 403
 
 
-@app.route('/users/<string:userid>/update', methods=['PATCH'])
-def update_user(userid): #TODO: Update new value in DB to track 
+@app.route('/users/<string:user_id>', methods=['PUT', 'PATCH'])
+def update_user(user_id):  # TODO: Update new value in DB to track
     data = request.json
-    s =""
+    s = ""
 
-    for n in data :
+    for n in data:
         a = data[n]
-        print(n,"è",a,sep=" ")
-        s = ( "%s<br>%s  è %s " ) % (s, n, a)
+        print(n, "è", a, sep=" ")
+        s = ("%s<br>%s  è %s ") % (s, n, a)
     return s, 200
 
 
-@app.route('/')
+@app.route('/waste_bins/<string:waste_bin_id>', methods=['PUT', 'PATCH'])
+def update_waste_bin(waste_bin_id):  # TODO: Update new value in DB to track
+    data = request.json
+    s = ""
+
+    for n in data:
+        a = data[n]
+        print(n, "è", a, sep=" ")
+        s = ("%s<br>%s  è %s ") % (s, n, a)
+    return s, 200
+
+
+@app.route('/hello')
 def hello_world():
     return send_from_directory(directory='../static', filename='example.html')
 
