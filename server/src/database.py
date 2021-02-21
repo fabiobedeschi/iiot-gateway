163d4be92405671e811f2ae3f9bfcf170b6b2408
Fabio Bedeschi <fabiobedeschi5@gmail.com>
2021-02-21 18:43:52 +0100

    improve api response

--- server/src/database.py
+++ server/src/database.py
@@ -1,6 +1,8 @@
 from logging import getLogger
 from os import getenv
+
 from psycopg2 import connect, Error
+from psycopg2.extras import RealDictCursor, RealDictRow
 
 logger = getLogger()
 
@@ -21,87 +23,93 @@ class Database:
         }
         return connect(**connection_conf)
 
-    def open_cursor(self):
-        self.cursor = self.connection.cursor()
+    def _open_cursor(self):
+        self.cursor = self.connection.cursor(cursor_factory=RealDictCursor)
 
-    def close_cursor(self):
+    def _close_cursor(self):
         if self.cursor:
             self.cursor.close()
 
-    def commit(self):
-        self.close_cursor()
+    def _commit(self):
+        self._close_cursor()
         self.connection.commit()
 
-    def rollback(self):
-        self.close_cursor()
+    def _rollback(self):
+        self._close_cursor()
         self.connection.rollback()
 
-    def execute_query(self, sql, values=None, fetch_all=False):
-        self.open_cursor()
+    def _execute_query(self, sql, values=None, fetch_all=False):
+        self._open_cursor()
         try:
             self.cursor.execute(sql, values)
             result = self.cursor.fetchall() if fetch_all else self.cursor.fetchone()
-            self.commit()
+            self._commit()
             return result
 
         except Error as e:
             logger.exception(f'Failed to execute query "{sql}" with values "{values}"')
-            self.rollback()
+            self._rollback()
             raise e
 
-    def find_user(self, uuid):
+    def find_all_users(self) -> [RealDictRow]:
+        sql = '''
+            SELECT * FROM users
+        '''
+        return self._execute_query(sql, fetch_all=True)
+
+    def find_user(self, uuid) -> RealDictRow:
         sql = '''
             SELECT * FROM users
             WHERE uuid = %(uuid)s
         '''
         values = {'uuid': uuid}
-        return self.execute_query(sql, values)
+        return self._execute_query(sql, values)
 
-    def update_user(self, uuid, delta):
+    def update_user(self, uuid, delta) -> RealDictRow:
         sql = '''
             UPDATE users
             SET delta = delta + %(delta)s, updated_at = NOW()
             WHERE uuid = %(uuid)s
-            RETURNING uuid
+            RETURNING *
         '''
         values = {
             'uuid': uuid,
             'delta': delta
         }
-        return self.execute_query(sql, values)
+        return self._execute_query(sql, values)
 
-    def update_waste_bin(self, uuid, fill_level):
+    def update_waste_bin(self, uuid, fill_level) -> RealDictRow:
         sql = '''
             UPDATE waste_bins
             SET fill_level = %(fill_level)s, updated_at = NOW()
             WHERE uuid = %(uuid)s
-            RETURNING uuid
+            RETURNING *
         '''
         values = {
             'uuid': uuid,
             'fill_level': fill_level
         }
-        return self.execute_query(sql, values)
+        return self._execute_query(sql, values)
 
     # TODO: remove this before delivery
-    def insert_user(self, uuid, delta=0):
+    def insert_user(self, uuid, delta=0) -> RealDictRow:
         sql = '''
             INSERT INTO users (uuid, delta)
             VALUES (%(uuid)s, %(delta)s)
-            RETURNING uuid
+            RETURNING *
         '''
         values = {
             'uuid': uuid,
             'delta': delta
         }
-        return self.execute_query(sql, values)
+        return self._execute_query(sql, values)
 
     # TODO: remove this before delivery
-    def delete_user(self, uuid):
+    def delete_user(self, uuid) -> RealDictRow:
         sql = '''
             DELETE FROM users
             WHERE uuid = %(uuid)s
-            RETURNING uuid
+            RETURNING *
         '''
         values = {'uuid': uuid}
-        return self.execute_query(sql, values)
+        return self._execute_query(sql, values)
--- server/src/main.py
+++ server/src/main.py
@@ -1,16 +1,25 @@
-from flask import Flask, request
+from typing import Optional
+
+from flask import Flask, request, jsonify
 from flask.helpers import send_from_directory
 
 from .database import Database
 
 app = Flask(__name__)
-db = Database()
+db: Optional[Database] = None
+
+
+@app.before_request
+def init_app():
+    global db
+    if not db:
+        db = Database()
 
 
 @app.route('/users/<string:user_id>', methods=['GET'])
 def check_user(user_id):
     result = db.find_user(user_id)
-    return user_id, (200 if result else 404)
+    return (jsonify(dict(result)), 200) if result else (None, 404)
 
 
 @app.route('/users/<string:user_id>', methods=['PUT', 'PATCH'])
@@ -21,7 +30,7 @@ def update_user(user_id):
             uuid=user_id,
             delta=data.get('delta', 0)
         )
-    return user_id, (200 if result else 404)
+    return (jsonify(dict(result)), 200) if result else (None, 404)
 
 
 @app.route('/waste_bins/<string:waste_bin_id>', methods=['PUT', 'PATCH'])
@@ -32,7 +41,7 @@ def update_waste_bin(waste_bin_id):
             uuid=waste_bin_id,
             fill_level=data.get('fill_level'),
         )
-    return waste_bin_id, (200 if result else 404)
+    return (jsonify(dict(result)), 200) if result else (None, 404)
 
 
 # TODO: remove this before delivery
@@ -47,7 +56,7 @@ def create_user(user_id):
     result = None
     if check_user(user_id)[1] == 404:
         result = db.insert_user(user_id)
-    return user_id, (201 if result else 409)
+    return (jsonify(dict(result)), 201) if result else (None, 409)
 
 
 # TODO: remove this before delivery
@@ -56,7 +65,7 @@ def remove_user(user_id):
     result = None
     if check_user(user_id)[1] == 200:
         result = db.delete_user(user_id)
-    return user_id, (200 if result else 404)
+    return (jsonify(dict(result)), 200) if result else (None, 404)
 
 
 if __name__ == '__main__':
